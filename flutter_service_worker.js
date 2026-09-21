781cfa4b411d808ee3fae20c1e417ac294b6b0cd
kazweda <kazweda@users.noreply.github.com>
2026-09-21 14:39:52 +0000

    deploy: 6de324118add7a4a7154751bae84814fadade1fb

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"manifest.json": "e12b57071bf8ab1068578d9d355d87f8",
 "version.json": "c1b6ba385501b89b860c3a715971a571",
 "main.dart.js": "13f0763384a2497096581434d041cae3",
-"flutter_bootstrap.js": "b4f7565c2bfdc818a6d5ef4aa2e21bbd",
+"flutter_bootstrap.js": "a951ef3462a149432702587f9414b933",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
