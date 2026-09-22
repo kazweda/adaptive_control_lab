5b89feed0a03ac2b4b7f745e9d1143dd38551792
kazweda <kazweda@users.noreply.github.com>
2026-09-22 00:59:19 +0000

    deploy: 8d5a6d5796a0b154752ede2520d3f57696dd1537

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "e12b57071bf8ab1068578d9d355d87f8",
-"version.json": "c1b6ba385501b89b860c3a715971a571",
-"main.dart.js": "13f0763384a2497096581434d041cae3",
-"flutter_bootstrap.js": "a951ef3462a149432702587f9414b933",
+"version.json": "26b620f752b1abff36a638921af7fd28",
+"main.dart.js": "f981c0f98fdd8d1664a43901a64262b4",
+"flutter_bootstrap.js": "644df7f49e20c967510da834e9f0b1f3",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -33,7 +33,7 @@ const RESOURCES = {"manifest.json": "e12b57071bf8ab1068578d9d355d87f8",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b93248a553f9e8bc17f1065929d5934b",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/NOTICES": "f51566e7aec9b6f242cb9d39eb8f7ce0",
+"assets/NOTICES": "8f3c9bc0ed58f97d0a9280da65a50eaa",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e"};
 // The application shell files that are downloaded before a service worker can
 // start.
